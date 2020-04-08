a1fe7fe83e6943abb7f6e9f838ecc7dd0286b308
David S. García (Azæl) <azael@dragonspark.tech>
2020-04-07 22:49:53 -0500

    chore(@halogen/cli): create tools index

---
+++ packages/halogen/cli/src/tools/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * Copyright © DragonSpark Technologies - 2020. All Rights Reserved.
+ *
+ * This source code is licensed under the Apache-2.0 license found
+ * in the LICENSE file in the root directory of this source tree.
+ */
+
+export * from './argument-cleaner';
+export * from './index-finder';
