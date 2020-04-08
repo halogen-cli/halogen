b4f0dfc1a354568f552ab2f6c754f2060dbeb323
David S. García (Azæl) <azael@dragonspark.tech>
2020-04-07 22:47:12 -0500

    chore(@halokit/bundler): create custom rollup wrapper

---
+++ packages/halokit/bundler/rollup.config.js
@@ -0,0 +1,65 @@
+import path from 'path';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
+import ts from '@wessberg/rollup-plugin-ts';
+import TypeScript from 'typescript';
+
+const packageFolder = path.resolve(process.cwd());
+const packagePath = path.resolve(packageFolder + '/package.json');
+const pkg = require(packagePath);
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      format: 'esm',
+      file: path.join(packageFolder, 'dist/es/index.js')
+    },
+    {
+      format: 'cjs',
+      file: path.join(packageFolder, 'dist/lib/index.js')
+    }
+  ],
+  external: ['@halokit/reporter', '@rollup/plugin-commonjs', '@rollup/plugin-node-resolve', '@wessberg/rollup-plugin-ts', 'change-case', 'fs-extra', 'glob', 'path', 'rollup', 'typescript'],
+  plugins: [
+    resolve({
+      extensions,
+      preferBuiltins: true
+    }),
+    commonjs(),
+    json(),
+    ts({
+      transpiler: 'babel',
+      exclude: ["**/node_modules/**/*.*"],
+      typescript: TypeScript,
+      tsconfig: {
+        target: "ESNext",
+        module: "CommonJS",
+        lib: [
+          "ESNext",
+          "DOM"
+        ],
+        noImplicitReturns: true,
+        noUnusedLocals: true,
+        outDir: "output",
+        sourceMap: true,
+        strict: true,
+        noFallthroughCasesInSwitch: true,
+        moduleResolution: "node",
+        allowSyntheticDefaultImports: true,
+        esModuleInterop: true,
+        forceConsistentCasingInFileNames: true,
+        emitDecoratorMetadata: true,
+        experimentalDecorators: true,
+        declaration: true,
+        removeComments: true,
+        baseUrl: "./packages",
+        incremental: false,
+        resolveJsonModule: true
+      }
+    })
+  ]
+};
