7ada73508e23577bd18918ae7ccd6cd74f96a515
David S. García (Azæl) <azael@dragonspark.tech>
2020-04-07 22:48:10 -0500

    chore(@halokit/bundler): create bundlers index

---
+++ packages/halokit/bundler/src/bundlers/index.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright © DragonSpark Technologies - 2020. All Rights Reserved.
+ *
+ * This source code is licensed under the Apache-2.0 license found
+ * in the LICENSE file in the root directory of this source tree.
+ */
+
+import { TSBundler } from './ts-bundler';
+import { TSBrowserBundler } from './ts-browser-bundler';
+
+export const bundlers = new Map([
+  ['ts', TSBundler],
+  ['ts-browser', TSBrowserBundler]
+]);
